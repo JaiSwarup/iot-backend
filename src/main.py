4b21ad170099a213cc98e69ffbeac653f80b84f0
jaiswarup0 <jaiswarup0@gmail.com>
2025-04-16 12:49:44 +0000

    add support for user data

--- src/main.py
+++ src/main.py
@@ -7,6 +7,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+data = {
+  'C12B273': {
+    'name': 'Sonu',
+    'backlog': 100,
+    'overtime': 100,
+    'credits': 100,
+    'idle_time': 20,
+  },
+  '59BAAF2': {
+    'name': 'Rahul',
+    'backlog': 22,
+    'overtime': 1,
+    'credits': 400,
+    'idle': 0,
+  }
+}
+
 class ProductivityModel:
   """
   Predicting an employee's productivity
@@ -15,12 +32,12 @@ class ProductivityModel:
     self.model = joblib.load('models/rf_model.joblib')
     pass
   
-  def predict(self, id : int):
+  def predict(self, id : str):
     data = {
-      'wip': [100],
-      'over_time': [100],
-      "incentive": [100],
-      "idle_time": [1]
+      'wip': data[id]['backlog'],
+      'over_time': data[id]['overtime'],
+      "incentive": data[id]['credits'],
+      "idle_time": data[id]['idle']
     }
     return self.model.predict(pd.DataFrame(data))
 
@@ -35,8 +52,24 @@ def hello_world():
   except Exception as e: 
     return e.__str__()
 
+@app.get("/users")
+def get_users():
+  """Get user data."""
+  try: 
+    return data
+  except Exception as e: 
+    return e.__str__()
+
+@app.get("/users/{id}")
+def get_user_data(id: str):
+  """Get user data."""
+  try: 
+    return data[id]
+  except Exception as e: 
+    return e.__str__()
+
 @app.post("/predict")
-def predict_productivity(id: int):
+def predict_productivity(id: str):
   """
   Predicting an employee's productivity
   """
